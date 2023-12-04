db433fe87bc5165bbd2170ac86b9736dba601b14
Przemyslaw Koziel <mos.koziel@gmail.com>
2023-12-04 09:44:07 +0100

    Styling

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      "kg-purple": "#635FC7",
+      "kg-purple-hover": "#A8A4FF",
+      "kg-red": "#EA5555",
+      "kg-red-hover": "#FF9898",
+      "kg-black": "#000112",
+      "kg-grey-very-dark": "#20212C",
+      "kg-grey-dark": "#2B2C37",
+      "kg-grey-medium": "#828FA3",
+      "kg-grey-light": "#F4F7FD",
+      "kg-lines-dark": "#3E3F4E",
+      "kg-lines-light": "#E4EBFA",
+      "kg-white": "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
