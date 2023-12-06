eb997da43e3e48c10e81290632f9a401298c926a
Przemyslaw Koziel <mos.koziel@gmail.com>
2023-12-06 16:12:39 +0100

    Retrieving data from store in SidePanel

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromBoards from '../store/boards/boards.reducer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.scss',
 })
 export class SidePanelComponent {
-  public boards = ['Platform Launch', 'Marketing Plan', 'Roadmap'];
+  public boards$ = this.store.select(fromBoards.selectorBoardNames);
+  constructor(private store: Store<fromBoards.State>) {}
 }
--- src/app/store/boards/boards.reducer.ts
+++ src/app/store/boards/boards.reducer.ts
@@ -1,7 +1,26 @@
-import { createReducer } from '@ngrx/store';
+import {
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+} from '@ngrx/store';
 import * as data from './../../../assets/data.json';
 import { Board } from './boards';
 
-export const initialState: Board[] = data.boards;
+export interface State {
+  boards: Board[];
+}
+
+export const initialState: State = {
+  boards: data.boards,
+};
 
 export const boardsReducer = createReducer(initialState);
+
+export const selectorBoardState = createFeatureSelector<State>('boards');
+
+export const selectorBoardNames = createSelector(
+  selectorBoardState,
+  (state) => {
+    return state.boards.map((board) => board.name);
+  }
+);
